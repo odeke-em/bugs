369a53cdf23c0d2cd443bbe5531a6705787583b6
Emmanuel Odeke <emm.odeke@gmail.com>
2015-02-26 20:24:02 -0700

    vim: incorrect braces within comments

---
+++ vim/braces_in_comments/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    // for (i = 0; i < 10; ++i) {
+    printf("hello world!\n");
+    return 0;
+}
---
+++ vim/braces_in_comments/hello.js
@@ -0,0 +1,4 @@
+function main() {
+    // for (var i = 0; i < 10; ++) {
+    console.log('Hello world!');
+}
